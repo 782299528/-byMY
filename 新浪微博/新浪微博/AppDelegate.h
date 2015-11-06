61a1d8383845cb21bc93290f72cf5be0d3df2fc4
782299528 <13693125668@163.com>
2015-11-06 17:48:19 +0800

    开启新浪微博

---
+++ 新浪微博/新浪微博/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  新浪微博
+//
+//  Created by mac on 15/11/6.
+//  Copyright © 2015年 miaoyi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
